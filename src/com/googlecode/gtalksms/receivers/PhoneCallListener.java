b118bb26d2b42f52390ed8af222267076334cc60
Florian Schmaus <fschmaus@gmail.com>
2011-03-30 16:55:05 +0200

    re-added manageIncoming boolean to prevent multiple "is calling" notifications

--- src/com/googlecode/gtalksms/receivers/PhoneCallListener.java
+++ src/com/googlecode/gtalksms/receivers/PhoneCallListener.java
@@ -19,13 +19,31 @@ public class PhoneCallListener extends PhoneStateListener {
 
     private MainService svc;
     private SettingsManager settingsMgr;
+    
+    // android seems to send the intent not only once per call
+    // but every 10 seconds for ongoing ringing
+    // we prevent multiple "is calling" notifications with this boolean
+    private static boolean manageIncoming = true;
 
     public void onCallStateChanged(int state, String incomingNumber) {
-        if (state == TelephonyManager.CALL_STATE_RINGING) {
-            if(settingsMgr.debugLog) 
-                Log.d(Tools.LOG_TAG, "PhoneCallListener Call State Ringing with incomingNumber: " + incomingNumber);
-            String contact = ContactsManager.getContactName(svc, incomingNumber);
-            svc.send(svc.getString(R.string.chat_is_calling, contact), null);
+        switch (state) {
+        case TelephonyManager.CALL_STATE_IDLE:
+            manageIncoming = true;
+            break;
+        case TelephonyManager.CALL_STATE_OFFHOOK:
+            manageIncoming = true;
+            break;
+        case TelephonyManager.CALL_STATE_RINGING:
+            if (settingsMgr.debugLog)
+                Log.d(Tools.LOG_TAG, "PhoneCallListener Call State Ringing with incomingNumber=" + incomingNumber + " manageIncoming=" + manageIncoming);
+            if (manageIncoming) {
+                manageIncoming = false;
+                String contact = ContactsManager.getContactName(svc, incomingNumber);
+                svc.send(svc.getString(R.string.chat_is_calling, contact), null);
+            }
+            break;
+        default:
+            break;
         }
     }
 }
