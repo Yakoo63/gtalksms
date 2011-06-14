3eb3bacbb119a6b9ee9a70d5fc255a701684eee7
Florian Schmaus <fschmaus@gmail.com>
2011-06-14 17:50:06 +0200

    Added BluetoothStateReceiver Adjusted Changelog Fixed small typo

--- src/com/googlecode/gtalksms/cmd/BluetoothCmd.java
+++ src/com/googlecode/gtalksms/cmd/BluetoothCmd.java
@@ -1,8 +1,11 @@
 package com.googlecode.gtalksms.cmd;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
 
 import com.googlecode.gtalksms.MainService;
+import com.googlecode.gtalksms.cmd.bluetoothCmd.BluetoothStateReceiver;
 
 public class BluetoothCmd extends CommandHandlerBase {
     
@@ -27,7 +30,8 @@ public class BluetoothCmd extends CommandHandlerBase {
     
     private void enableAdapter() {
         if (sBluetoothAdapter.enable()) {
-            send("bluetooth adapter startup has begun");
+            send("Bluetooth adapter startup has begun");
+            addBroadcastReceiver();
         } else {
             sendFailure();
         }
@@ -35,7 +39,8 @@ public class BluetoothCmd extends CommandHandlerBase {
     
     private void disableAdapter() {
         if (sBluetoothAdapter.disable()) {
-            send("Bluetooth adapter shutdown has begun"); 
+            send("Bluetooth adapter shutdown has begun");
+            addBroadcastReceiver();
         } else {
             sendFailure();
         }
@@ -44,29 +49,40 @@ public class BluetoothCmd extends CommandHandlerBase {
     private void showState() {
         int state = sBluetoothAdapter.getState();
         String res = "Bluetooth adapter is ";
-
-        switch(state) {
-        case BluetoothAdapter.STATE_OFF:
-            res += "off";
-            break;
-        case BluetoothAdapter.STATE_ON:
-            res += "on";
-            break;
-        case BluetoothAdapter.STATE_TURNING_OFF:
-            res += "turning off";
-            break;
-        case BluetoothAdapter.STATE_TURNING_ON:
-            res += "turning on";
-            break;
-        default:
-            throw new IllegalStateException();        
-        }
+        res += stateInt2String(state);
         send(res);
     }
     
     private void sendFailure() {
         send("Error on Bluetooth adapter");
     }
+    
+    private void addBroadcastReceiver() {
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        BroadcastReceiver br = new BluetoothStateReceiver(sMainService, mAnswerTo);
+        sContext.registerReceiver(br, filter);
+    }
+    
+    public static String stateInt2String(int state) {
+        String res;
+        switch(state) {
+        case BluetoothAdapter.STATE_OFF:
+            res = "off";
+            break;
+        case BluetoothAdapter.STATE_ON:
+            res = "on";
+            break;
+        case BluetoothAdapter.STATE_TURNING_OFF:
+            res = "turning off";
+            break;
+        case BluetoothAdapter.STATE_TURNING_ON:
+            res = "turning on";
+            break;
+        default:
+            throw new IllegalStateException();        
+        }
+        return res;
+    }   
     
     @Override
     public String[] help() {
---
+++ src/com/googlecode/gtalksms/cmd/bluetoothCmd/BluetoothStateReceiver.java
@@ -0,0 +1,36 @@
+package com.googlecode.gtalksms.cmd.bluetoothCmd;
+
+import com.googlecode.gtalksms.MainService;
+import com.googlecode.gtalksms.cmd.BluetoothCmd;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BluetoothStateReceiver extends BroadcastReceiver {
+    
+    private MainService mMainService;
+    private String mAnswerTo;
+    
+    public BluetoothStateReceiver(MainService mainService, String answerTo) {
+        this.mMainService = mainService;
+        this.mAnswerTo = answerTo;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1);
+        int prev_state = intent.getIntExtra(BluetoothAdapter.EXTRA_PREVIOUS_STATE, -1);
+        String stateStr = BluetoothCmd.stateInt2String(state);
+        String prev_stateStr = BluetoothCmd.stateInt2String(prev_state);
+        
+        send("Bluetooth Adapter state changed from \"" + prev_stateStr + "\" to \"" + stateStr + "\"");
+        context.unregisterReceiver(this);
+    }
+    
+    private void send(String msg) {
+        mMainService.send(msg, mAnswerTo);
+    }
+
+}
