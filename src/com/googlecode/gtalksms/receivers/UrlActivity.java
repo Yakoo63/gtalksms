22a1f1f00a29b1ad978aff3ceed62aa3e472a19b
Florian Schmaus <fschmaus@gmail.com>
2011-07-29 10:48:57 +0200

    Fixed URL/URI handling for urls that have parameters e.g. http://www.geocaching.com/seek/cache_details.aspx ->  http://www.geocaching.com/seek/cache_details.aspx?wp=GC2MDNE

--- src/com/googlecode/gtalksms/receivers/UrlActivity.java
+++ src/com/googlecode/gtalksms/receivers/UrlActivity.java
@@ -1,40 +1,21 @@
 package com.googlecode.gtalksms.receivers;
 
-import java.net.URL;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 
-import com.googlecode.gtalksms.R;
 import com.googlecode.gtalksms.tools.Tools;
 
 public class UrlActivity extends Activity {
-    Handler _handler = new Handler();
-    Runnable _exitRunnable = new Runnable() {
-        public void run() {
-            onDestroy();
-        }
-    };
-    
-    /** Called when the activity is first created. */
-    @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
         
-        // To get the data use
-        Uri data = intent.getData();
-        URL url;
-        try {
-            url = new URL(data.getScheme(), data.getHost(), data.getPath());
+        Intent intent = getIntent();       
+        Uri uri = intent.getData();
+        Tools.send(uri.toString(), null, this);
 
-            Tools.send(url.toString(), null, this);
-        } catch (Exception e) {
-            Tools.send(getString(R.string.chat_error, e.toString()), null, this);
-        }
         finish();
     }
 }
