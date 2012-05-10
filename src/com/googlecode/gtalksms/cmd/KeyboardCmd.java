7b66914fd797ac6e95951ffe37f3b3e66138650c
Florent L <none@none>
2012-05-10 22:43:07 +0200

    - Adding "wappend" and "wsend" commands with associated help.         wappend : add text to the current field         wsend : set the text and validate the field. Only validate if the text is empty.
    - Change "\n" by a cariage return in the field.

--- src/com/googlecode/gtalksms/cmd/KeyboardCmd.java
+++ src/com/googlecode/gtalksms/cmd/KeyboardCmd.java
@@ -6,20 +6,32 @@ import com.googlecode.gtalksms.R;
 
 public class KeyboardCmd extends CommandHandlerBase {
     public KeyboardCmd(MainService mainService) {
-        super(mainService, CommandHandlerBase.TYPE_COPY, new Cmd("write", "w"));
+        super(mainService, CommandHandlerBase.TYPE_COPY, new Cmd("write", "w"), new Cmd("wappend", "wa"), new Cmd("wsend", "ws", "wl"));
     }
     
     @Override
     protected void execute(String cmd, String args) {
         KeyboardInputMethod keyboard = sMainService.getKeyboard();
+        String msg = args.replace("\\n", "\n");
         
         if (keyboard != null) {
-            keyboard.setText(args);
+            if (isMatchingCmd("write", cmd)) {
+                keyboard.setText(msg);
+            } else if (isMatchingCmd("wappend", cmd)) {
+                keyboard.setText(keyboard.getText() + msg);
+            } else if (isMatchingCmd("wsend", cmd)) {
+                if (msg.length() > 0) {
+                    keyboard.setText(msg);
+                }
+                keyboard.sendDefaultEditorAction(false);
+            }
         }
     }
     
     @Override
     protected void initializeSubCommands() {
         mCommandMap.get("write").setHelp(R.string.chat_help_write, "#text#");   
+        mCommandMap.get("wappend").setHelp(R.string.chat_help_write_append, "#text#");   
+        mCommandMap.get("wsend").setHelp(R.string.chat_help_write_send, "#text#");   
     }
 }
