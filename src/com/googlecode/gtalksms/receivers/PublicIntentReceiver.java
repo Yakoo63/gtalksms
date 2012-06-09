09323a23f3d49dd0da6719bc54bc4535702241b6
Florent L <none@none>
2012-06-09 20:16:47 +0200

    Add disconnect intent as Public Fix NPE in PublicIntentReceiver Update donors

--- src/com/googlecode/gtalksms/receivers/PublicIntentReceiver.java
+++ src/com/googlecode/gtalksms/receivers/PublicIntentReceiver.java
@@ -14,12 +14,13 @@ public class PublicIntentReceiver extends BroadcastReceiver {
     private static IntentFilter sIntentFilter;
     private static Context sContext;
     
-    private SettingsManager mSettings;
+    private static SettingsManager sSettings;
 
     static {
         sIntentFilter = new IntentFilter();
         // ACTION_CONNECT and TOGGLE is received by filter within the Apps Manifest
         // sIntentFilter.addAction(MainService.ACTION_CONNECT);
+        // sIntentFilter.addAction(MainService.ACTION_DISCONNECT);
         // sIntentFilter.addAction(MainService.ACTION_TOGGLE);
         sIntentFilter.addAction(MainService.ACTION_COMMAND);
         sIntentFilter.addAction(MainService.ACTION_SEND);
@@ -34,7 +35,7 @@ public class PublicIntentReceiver extends BroadcastReceiver {
     }
 
     private PublicIntentReceiver(Context context) {
-        mSettings = SettingsManager.getSettingsManager(context);
+        sSettings = SettingsManager.getSettingsManager(context);
         sContext = context;
     }
     
@@ -62,13 +63,14 @@ public class PublicIntentReceiver extends BroadcastReceiver {
         Log.d("PublicIntentReceiver got intent: " + intent.getAction());
 
         // Init the static class to have access to the SettingsManager
-        if (sPublicIntentReceiver == null)
+        if (sPublicIntentReceiver == null) {
             sPublicIntentReceiver = new PublicIntentReceiver(context);
+        }
 
-        String token = intent.getStringExtra("token");
-        if (mSettings.publicIntentsEnabled) {
-            if (mSettings.publicIntentTokenRequired) {
-                if (token == null || !mSettings.publicIntentToken.equals(token)) {
+        if (sSettings.publicIntentsEnabled) {
+            if (sSettings.publicIntentTokenRequired) {
+                String token = intent.getStringExtra("token");
+                if (token == null || !sSettings.publicIntentToken.equals(token)) {
                     // token required but no token set or it doesn't match
                     Log.w("Public intent without correct security token received");
                     return;
