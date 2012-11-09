bdb883e3bc71fa032b342c0e1fdb5b4833f6603b
Florian Schmaus <fschmaus@gmail.com>
2012-11-09 21:10:15 +0100

    Added files needed to get ABS running with ant

---
+++ initProject.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FDIR='files'
+ABSDIR='external-libs/ActionBarSherlock/library'
+
+if [ -f local.properties ] ; then
+    cp local.properties $ABSIDR/
+else
+    echo Warning, please create your local properties with the sdk location
+    echo See local.properties.example
+    exit 1
+fi
+
+cp $FDIR/abs-build.xml $ABSDIR/build.xml
+cp $FDIR/abs-AndroidManifest.xml $ABSDIR/AndroidManifest.xml
+cp $FDIR/abs-project.properties $ABSDIR/project.properties
+cp $FDIR/abs-dot-project $ABSDIR/.project
