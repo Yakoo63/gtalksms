ceb7f18378f4c455854e0411058b02b68c6ff6b9
Florian Schmaus <fschmaus@gmail.com>
2012-11-11 17:11:38 +0100

    initProject: fix permissions and bash variable

--- initProject.sh
+++ initProject.sh
@@ -4,7 +4,7 @@ FDIR='files'
 ABSDIR='external-libs/ActionBarSherlock/library'
 
 if [ -f local.properties ] ; then
-    cp local.properties $ABSIDR/
+    cp local.properties $ABSDIR/
 else
     echo Warning, please create your local properties with the sdk location
     echo See local.properties.example
