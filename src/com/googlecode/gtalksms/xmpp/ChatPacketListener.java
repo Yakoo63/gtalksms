3ebcdfffbded49d3867a74927a97962ce31ba346
Florian Schmaus <fschmaus@gmail.com>
2013-01-24 12:17:21 +0100

    ChatPacketListener: Code style cleanup

--- src/com/googlecode/gtalksms/xmpp/ChatPacketListener.java
+++ src/com/googlecode/gtalksms/xmpp/ChatPacketListener.java
@@ -13,31 +13,34 @@ import com.googlecode.gtalksms.SettingsManager;
 import com.googlecode.gtalksms.tools.Tools;
 
 public class ChatPacketListener implements PacketListener {
-        private SettingsManager mSettings;
-        private Context mCtx;
-        
+	private SettingsManager mSettings;
+	private Context mCtx;
+
 	public ChatPacketListener(Context ctx) {
-            this.mCtx = ctx;
-            this.mSettings = SettingsManager.getSettingsManager(ctx);
-        }
-        
-        public void processPacket(Packet packet) {
-            Message message = (Message) packet;
-            String from = message.getFrom();
-            
+		this.mCtx = ctx;
+		this.mSettings = SettingsManager.getSettingsManager(ctx);
+	}
+
+	public void processPacket(Packet packet) {
+		Message message = (Message) packet;
+		String from = message.getFrom();
+
 		if (mSettings.startWithNotifiedAddress(from) && message.getBody() != null) {
-                if (mSettings.debugLog) {
-                    Log.i(Tools.LOG_TAG, "XMPP packet received - sending Intent: " + MainService.ACTION_XMPP_MESSAGE_RECEIVED);
-                }
-                
-                Tools.startSvcXMPPMsg(mCtx, message.getBody(), from);
-            } else if (mSettings.debugLog) {
-                if (!mSettings.startWithNotifiedAddress(from)) {
-                    Log.i(Tools.LOG_TAG, "XMPP packet received - but from address \"" + from.toLowerCase() + "\" does not match notification address \"" 
-                            + TextUtils.join("|",mSettings.getNotifiedAddresses()));
-                } else if (message.getBody() == null) {
-                    Log.i(Tools.LOG_TAG, "XMPP Packet received - but without body (body == null)");
-                }
-            }
-        }
+			if (mSettings.debugLog) {
+				Log.i(Tools.LOG_TAG, "XMPP packet received - sending Intent: "
+				        + MainService.ACTION_XMPP_MESSAGE_RECEIVED);
+			}
+
+			Tools.startSvcXMPPMsg(mCtx, message.getBody(), from);
+		} else if (mSettings.debugLog) {
+			if (!mSettings.startWithNotifiedAddress(from)) {
+				Log.i(Tools.LOG_TAG,
+				        "XMPP packet received - but from address \"" + from.toLowerCase()
+				                + "\" does not match notification address \""
+				                + TextUtils.join("|", mSettings.getNotifiedAddresses()));
+			} else if (message.getBody() == null) {
+				Log.i(Tools.LOG_TAG, "XMPP Packet received - but without body (body == null)");
+			}
+		}
+	}
 }
