3fc90ae71d0de9c8bef866b5e7e3ffafe77447ee
Florent L <yakoo63@gmail.com>
2014-03-13 00:23:56 +0100

    Updating project

--- initProject.bat
+++ initProject.bat
@@ -4,7 +4,7 @@ set ABSDIR=external-libs\ActionBarSherlock\actionbarsherlock
 if not exist %ABSDIR% (
 	mkdir .git\modules\external-libs
 	mkdir external-libs
-	rem git submodule update --init --recursive
+	
 	echo Initializing Module
 	git submodule add -f git://github.com/JakeWharton/ActionBarSherlock.git external-libs\ActionBarSherlock
 	echo Copy Android Studio configuration
