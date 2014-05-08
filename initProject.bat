75eb104b7b4e243bd19366e11d490d7804c47651
Florent L <yakoo63@gmail.com>
2014-05-09 01:19:31 +0200

    Project build updated to gradle

--- initProject.bat
+++ initProject.bat
@@ -1,16 +1,27 @@
 @echo off
-set ABSDIR=external-libs\ActionBarSherlock\actionbarsherlock
+set ABSDIR=external-libs\ActionBarSherlock
+set MTMDIR=external-libs\MemorizingTrustManager
+
+mkdir .git\modules\external-libs
+mkdir external-libs
 
 if not exist %ABSDIR% (
-	mkdir .git\modules\external-libs
-	mkdir external-libs
-	
-	echo Initializing Module
-	git submodule add -f git://github.com/JakeWharton/ActionBarSherlock.git external-libs\ActionBarSherlock
+	echo Initializing Module ActionBarSherlock
+	git submodule add -f git://github.com/JakeWharton/ActionBarSherlock.git/actionbarsherlock external-libs\ActionBarSherlock
 	echo Copy Android Studio configuration
-	cp actionbarsherlock.iml %ABSDIR%
+	cp external-libs\ActionBarSherlock.iml %ABSDIR%
 ) else (
-    echo Project already initialized
+    echo Project ActionBarSherlock already initialized
+)
+
+if not exist %MTMDIR% (
+	echo Initializing Module MemorizingTrustManager
+	git submodule add -f https://github.com/ge0rg/MemorizingTrustManager.git external-libs\MemorizingTrustManager
+	echo Copy Android Studio configuration
+	cp external-libs\MemorizingTrustManager.iml %MTMDIR%
+) else (
+    echo Project MemorizingTrustManager already initialized
 )
 
 pause
+
