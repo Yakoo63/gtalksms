1a27573c0278e38a89291f3d423a04380cdc075e
Florent L <yakoo63@gmail.com>
2015-03-07 00:53:29 +0100

    Fixes issue 379

--- src/com/googlecode/gtalksms/panels/KeyboardMainView.java
+++ src/com/googlecode/gtalksms/panels/KeyboardMainView.java
@@ -5,26 +5,25 @@ import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.inputmethodservice.Keyboard.Key;
 import android.util.AttributeSet;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
 public class KeyboardMainView extends KeyboardView {
 
-    private static final int KEYCODE_OPTIONS = -100;
+    public KeyboardMainView(Context context, AttributeSet attrs, int defStyle) {
+        this(context, attrs);
+    }
 
     public KeyboardMainView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public KeyboardMainView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
     @Override
     protected boolean onLongPress(Key key) {
-        if (key.codes[0] == Keyboard.KEYCODE_CANCEL) {
-            getOnKeyboardActionListener().onKey(KEYCODE_OPTIONS, null);
-            return true;
-        } else {
-            return super.onLongPress(key);
+        InputMethodManager  inputMethodManager = (InputMethodManager) getContext().getApplicationContext().getSystemService(getContext().INPUT_METHOD_SERVICE);
+        if (inputMethodManager != null) {
+            inputMethodManager.showInputMethodPicker();
         }
+        return true;
     }
 }
